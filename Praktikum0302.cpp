b4e4bdcc3cd3cb5eb110ac823ab7a2edc84c9595
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2017-10-31 04:39:22 +0800

    Menghitung dan Menampilkan nilai Persegi Panjang

--- Praktikum0302.cpp
+++ Praktikum0302.cpp
@@ -5,5 +5,10 @@ int main(){
 	int luas,l,alas,t;
 	char bidang;
 	cout << "jenis bidang (P atau S) ? "; cin >> bidang;
-	  
+	  if (bidang=='P'){
+	  	cout << "Nilai Tinggi  = "; cin >> t;
+		cout << "Nilai Lebar   = "; cin >> l;
+		luas = t*l;
+		cout << "Luas Persegi Panjang adalah " << luas;
+	  }
 }
