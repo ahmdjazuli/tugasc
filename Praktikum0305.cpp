053c9142b9cacded5e6cfbe5fb947b24a3799fd2
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2017-11-02 02:22:38 +0800

    Percabangan else if utk menentukan apakah kamu Underweight/Normal/Overweight

--- Praktikum0305.cpp
+++ Praktikum0305.cpp
@@ -6,4 +6,11 @@ int main(){
 	cout << "===== PROGRAM TES BADAN IDEAL KAMU =====" << endl;
 	cout << "Berapa Tinggi badan kamu = "; cin >> tinggi;
 	cout << "Berapa Berat badan kamu  = "; cin >> berat;
+		if (berat<tinggi/2.5){
+			cout << "Anda kekurangan Berat Badan/Underweight";
+		}else if(tinggi/2.5 <= berat && berat <= tinggi/2.3){
+			cout << "Badan Kamu IDEAL :D normal body";
+		}else if(tinggi/2.3 < berat){
+			cout << "Badan Kamu terlalu Berat/Gemuk :p/Overweight";
+		}	
 }
