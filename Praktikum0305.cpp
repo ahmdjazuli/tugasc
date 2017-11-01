2557dc591cfc1a6388c50b695a3a604198339103
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2017-11-02 02:21:22 +0800

    Inputkan tinggi dan berat badan

--- Praktikum0305.cpp
+++ Praktikum0305.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 
 int main(){
-	
+	int tinggi,berat;
+	cout << "===== PROGRAM TES BADAN IDEAL KAMU =====" << endl;
+	cout << "Berapa Tinggi badan kamu = "; cin >> tinggi;
+	cout << "Berapa Berat badan kamu  = "; cin >> berat;
 }
