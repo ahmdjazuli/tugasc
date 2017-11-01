c6cb985654fffeb0d0863c8731728f8ba4d96a75
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2017-11-02 02:18:54 +0800

    Percabangan Else If utk menentukan bilangan itu positif,negatif/nol

--- Praktikum0304.cpp
+++ Praktikum0304.cpp
@@ -4,4 +4,11 @@ using namespace std;
 int main(){
 	int x;
 	cout << "Inputkan Angka = "; cin >> x;
+	if (x>0){
+			cout << x << " adalah Ini Bilangan Positif";
+		}else if(x<0){
+			cout << x << " adalah Ini Bilangan Negatif";
+		}else if(x==0){
+			cout << x << " adalah Ini Bilangan Nol";	
+		}
 }
