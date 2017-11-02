288f61661645c2576ffa2750e280419efe570934
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2017-11-03 04:16:20 +0800

    Membuat Percabangan untuk menghitung gaji bersih minggu pertama
    Pake Array...

--- Praktikum0303.cpp
+++ Praktikum0303.cpp
@@ -4,4 +4,13 @@ using namespace std;
 int main(){
 	int gajiJam = 20000, gajiPerminggu = 980000;
 	int waktu[3],gajiLembur,gajiAwal,gajiTotal[4];
+	
+		cout << "waktu anda bekerja minggu ke-1   = "; cin >> waktu[0];
+		if (waktu[0]>40){
+			gajiLembur=(waktu[0]-40)*1.5*gajiJam;
+			gajiTotal[0]=(gajiPerminggu+gajiLembur)*0.85;
+			gajiAwal=gajiPerminggu*0.85;
+		}else{
+			gajiTotal[0]=gajiPerminggu*0.85;
+		}
 }
