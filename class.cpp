edfe5fc3ea9902092488a70b6036f6f4bf72b8e9
JuliKoding(Ahmad Jazuli) <julikoding@gmail.com>
2018-01-19 04:52:06 +0800

    nampilin data pake percabangan if dan switch

--- class.cpp
+++ class.cpp
@@ -46,4 +46,41 @@ int main(){
 	//kapasitas Ram dari Smartphone
 	XiaomiRedmi5A.kapasitasRAM = 2;
 	iPhone8Plus.kapasitasRAM = 4;
+	
+	// Resolusi kamera smarthphone
+	XiaomiRedmi5A.rkameraDepan = 5;
+	XiaomiRedmi5A.rkameraBelakang = 12;
+	iPhone8Plus.rkameraDepan = 8;
+	iPhone8Plus.rkameraDepan = 20;
+	
+	cout << "|==== CEK SmartPhone sebelum mau BELI ====|"<<endl;
+	cout << "|========= 1. xiaomi | 2. iphone ======== |"<<endl;
+	cout << "|=========================================|"<<endl;
+	cout << "| Data Smartphone yang ingin kamu liat : ";cin>>liat;
+		if(liat=="1"){
+			cout << " Nama Smartphone    = Xiaomi Redmi 5A"<<endl;
+			cout << " Kamera Depan       = "<<XiaomiRedmi5A.rkameraDepan<<"MP"<<endl;
+			cout << " Kamera Belakang    = "<<XiaomiRedmi5A.rkameraBelakang<<"MP"<<endl;
+			cout <<" Kapasitas Ram       = "<<XiaomiRedmi5A.kapasitasRAM<<"GB"<<endl<<endl; //double
+			system("cls");listgame(); //memanggil fungsi listgame
+			sini:
+			cout << "Kamu mau maen Game apa : ";cin>>pp;
+				switch(pp){
+			case '1' :
+				XiaomiRedmi5A.maingame(mobilelegend, XiaomiRedmi5A);
+				break; // utk mengakhiri kondisi percabangan
+			case '2' :
+				XiaomiRedmi5A.maingame(tricky, XiaomiRedmi5A);
+				break;	
+			default :
+				cout << "Kode yang anda masukkan SALAH! pilih lagi."<< endl; goto sini;
+				} //akhir switch
+		}else if(liat=="2"){
+			cout << " Nama Smartphone = iPhone 8 Plus"<<endl;
+			cout << " Kamera Depan       = "<<iPhone8Plus.rkameraDepan<<"MP"<<endl;
+			cout << " Kamera Belakang    = "<<iPhone8Plus.rkameraBelakang<<"MP"<<endl;
+			cout <<" Kapasitas Ram   = " <<(iPhone8Plus.kapasitasRAM)<<"GB"<<endl; 
+		}else{
+			cout << "ERROR 405! Kode yang anda masukkan SALAH!";
+		}//akhir percabangan
 }
