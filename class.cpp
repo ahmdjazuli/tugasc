a9f09c7f8c6b1641d57a044e3339dfe5ae7b533b
JuliKoding(Ahmad Jazuli) <julikoding@gmail.com>
2018-01-19 04:43:12 +0800

    membuat class smartphone dan mainan

--- class.cpp
+++ class.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class mainan{
+	public:
+		double kapasitasRAM;
+};
+
+class smartphone{
+	public :
+		void maingame(mainan games, smartphone phone);
+		double kapasitasRAM;
+		int rkameraDepan;
+		int rkameraBelakang;
+};
+
 int main(){
 	
 }
